85a0edb725b9ebabf4d64d0e6a1bdba8e3212477
vidyakant <thakurvidyakant@gmail.com>
2025-08-22 18:20:22 +0530

    updated

---
+++ maxminVal.cpp
@@ -0,0 +1,30 @@
+// 2-> find the maximum and minimum value of the array
+
+#include<iostream>
+using namespace std;
+
+void maxminValue(int arr[] , int n){
+    // let us assume firt value of array is the maximum and minimum value 
+    int maxValue = arr[0];
+    int minValue = arr[0];
+
+    for(int i=1; i<n ; i++){
+        if(minValue < arr[i]){
+           minValue = arr[i]; // update the value of minValue
+        }
+        if(maxValue > arr[i]){
+            maxValue = arr[i]; // update the value of maxValue
+        }
+       
+    }
+        cout<< "minimum value is -> " << minValue << endl;
+        cout<< "maximum value is -> " << maxValue << endl;  
+}
+
+int main(){
+   int  arr[] = {64,9,36,51,33,40,10};
+
+   // here with the help of this formula we can calculate the size of an array ------ sizeof(arr) is the size of entire array 
+   int n = sizeof(arr)/sizeof(arr[0]);
+   maxminValue(arr, n);
+}
--- reverseArr.cpp
+++ reverseArr.cpp
@@ -21,7 +21,6 @@ void printArray(int arr[], int n){
 }
 
 
-
 int main(){
     int arr[9] = {1,2,3,4,5,6,7,8,9};
     int brr[9] = {9,8,7,6,5,4,3,2,1};
